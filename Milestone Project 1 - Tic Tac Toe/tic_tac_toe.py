de4fa6a5593ec4f4bea2e0fb9d1aafa698f6f386
Franklin Wu <franklin.w.wu@gmail.com>
2018-02-05 04:09:25 -0800

    Update board printing

--- Milestone Project 1 - Tic Tac Toe/tic_tac_toe.py
+++ Milestone Project 1 - Tic Tac Toe/tic_tac_toe.py
@@ -1,7 +1,7 @@
 board = [
-    [-1, 0, 0],
-    [0, 0, 0],
-    [0, 1, 0],    
+    -1, 0, 0,
+    0, 0, 0,
+    0, 1, 0,    
 ]
 
 turn = 1
@@ -15,9 +15,13 @@ def get_char(value):
         return ' '
 
 def print_board():
+    '''
     for row in board:
-        print '+-+-+-+'
         print '|{0}|{1}|{2}|\t\ta b c'.format(get_char(row[0]), get_char(row[1]), get_char(row[2]))
+    '''
+    for row in xrange(3):
+        print '+-+-+-+'
+        print '|' + '|'.join([get_char(board[(row * 3) + col]) for col in xrange(3)]) + '|' + '\t\t' + ' '.join([str((row * 3) + col + 1) for col in xrange(3)])
     print '+-+-+-+'
     print 'Player {0}, where do you want to place your token (1-9)?'.format(get_char(turn))
     print
