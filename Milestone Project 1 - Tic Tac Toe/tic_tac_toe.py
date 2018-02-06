9e2e8302b6ac3259f2fd97ebfca37614a2f27e59
Franklin Wu <franklin.w.wu@gmail.com>
2018-02-06 00:11:55 -0800

    Start winning board determination code

--- Milestone Project 1 - Tic Tac Toe/tic_tac_toe.py
+++ Milestone Project 1 - Tic Tac Toe/tic_tac_toe.py
@@ -10,6 +10,19 @@ def get_char(value):
     else:
         return ' '
 
+def get_winnable_lists_indices():
+    # Return the lists that if all O's or all X's constitute a win.
+    return [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ]
+
 def print_board():
     for row in xrange(3):
         print '+-+-+-+'
@@ -18,9 +31,16 @@ def print_board():
         print board_line
     print '+-+-+-+'
     print 'Player {0}, where do you want to place your token (1-9)?'.format(get_char(turn))
+    is_board_winning()
     print
 
+def is_board_winning():
+    winnable_lists = map(lambda indices: [board[index] for index in indices], get_winnable_lists_indices())
+    print winnable_lists
+
 def main():
+    print_board()
+    board[4] = 1
     print_board()
 
 if __name__ == '__main__':
