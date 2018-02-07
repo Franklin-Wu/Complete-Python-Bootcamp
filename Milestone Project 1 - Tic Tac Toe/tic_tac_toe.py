e7d8d02b852f9606c32c84c2d6983cd881e54be8
Franklin Wu <franklin.w.wu@gmail.com>
2018-02-07 03:26:05 -0800

    Start implementing turn

--- Milestone Project 1 - Tic Tac Toe/tic_tac_toe.py
+++ Milestone Project 1 - Tic Tac Toe/tic_tac_toe.py
@@ -1,6 +1,6 @@
 board = [0] * 9
 
-turn = 1
+turn_player = 1
 
 def get_char(value):
     if value == -1:
@@ -11,7 +11,8 @@ def get_char(value):
         return ' '
 
 def get_winnable_lists_indices():
-    # Return the lists that if all O's or all X's constitute a win.
+    # Return the lists that if all O's or all X's constitute a win, that is, the indices comprising
+    # each 3-cell row, column and diagonal.
     return [
         [0, 1, 2],
         [3, 4, 5],
@@ -30,38 +31,56 @@ def print_board():
         board_line += '   ' + ' '.join([str((row * 3) + col + 1) for col in xrange(3)])
         print board_line
     print '+-+-+-+'
-    print 'Player {0}, where do you want to place your token (1-9)?'.format(get_char(turn))
-    print get_winner()
-    print
 
 def get_winner():
     # Returns 'O', 'X' or None (no winner).
     #
     # This is probably _not_ the most efficient way to do this; it is meant to exercise map(),
     # reduce() and any().
+
+    # Get the contents of each 3-cell row, column and diagonal.
     winnable_lists = map(lambda indices: [board[i] for i in indices], get_winnable_lists_indices())
-    totals = map(lambda winnable_list: (reduce(lambda x, y: x + y, winnable_list)), winnable_lists)
-    if any(map(lambda total: total == -3, totals)):
+
+    # Get the sums of each 3-cell row, column and diagonal.
+    sums = map(lambda winnable_list: (reduce(lambda x, y: x + y, winnable_list)), winnable_lists)
+
+    if any(map(lambda total: total == -3, sums)):
+        # If any of the sums are -3, O wins.
         return 'O'
-    elif any(map(lambda total: total == 3, totals)):
+    elif any(map(lambda total: total == 3, sums)):
+        # If any of the sums are -3, X wins.
         return 'X'
+        # Otherwise nobody wins (yet).
     else:
         return None
 
+def get_turn_player_input():
+    pass
+
+def play_turn():
+    global turn_player
+    print_board()
+    print 'Player {0}, where do you want to place your token (1-9)?'.format(get_char(turn_player))
+    winner = get_winner()
+    print winner
+    print
+    turn_player *= -1
+    return winner
+
 def main():
-    print_board()
+    play_turn()
     board[4] = 1
-    print_board()
+    play_turn()
     board[2] = 1
-    print_board()
+    play_turn()
     board[6] = 1
-    print_board()
+    play_turn()
     board[6] = -1
-    print_board()
+    play_turn()
     board[0] = -1
-    print_board()
+    play_turn()
     board[3] = -1
-    print_board()
+    play_turn()
 
 if __name__ == '__main__':
     main()
